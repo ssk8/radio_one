9a1c10327e02d83d987675eb958d58dee3d93120
ssk8 <stinkyskate@pm.me>
2020-10-25 12:52:53 -0500

    code cleanup

--- radio.py
+++ radio.py
@@ -1,7 +1,10 @@
 import RPi.GPIO as GPIO
 from mplayer import Player
 from time import sleep
+from os import system
 
+music = "/home/pi/media/test2.mp3"
+#music = "https://radio.stereoscenic.com/asp-s"
 
 enc_pin_1, enc_pin_2 = 24, 23
 enc_button_pin, top_button_pin = 17, 27
@@ -9,24 +12,23 @@ last_encoded, encoder_value, last_encoder_value = 0, 70, 0
 enc_button_press, top_button_press = False, False
 
 
-def update_encoder(channel=0):
-    global encoder_value
-    global last_encoded
-    MSB = GPIO.input(enc_pin_1) == GPIO.LOW
-    LSB = GPIO.input(enc_pin_2) == GPIO.LOW
-    encoded = (MSB << 1) | LSB
-    summed = (last_encoded << 2) | encoded
-    if summed in (13, 4, 2, 11): encoder_value += 1
-    if summed in (14, 7, 1, 8): encoder_value -= 1
+def update_encoder(channel):
+    global encoder_value, last_encoded
+    encoded = ((GPIO.input(enc_pin_1) == GPIO.LOW) << 1) | (GPIO.input(enc_pin_2) == GPIO.LOW)
+    s = (last_encoded << 2) | encoded
+    encoder_value += (s in (13, 4, 2, 11)) - (s in (14, 7, 1, 8))
     last_encoded = encoded
 
 
-def enc_button_event(channel=0):
+def enc_button_event(channel):
     global enc_button_press
+    if GPIO.input(top_button_pin) == GPIO.LOW:
+        print("\nshutdown")
+        system('sudo shutdown now -h')
     enc_button_press = True
 
 
-def top_button_event(channel=0):
+def top_button_event(channel):
     global top_button_press
     top_button_press = True
 
@@ -35,9 +37,9 @@ GPIO.setmode(GPIO.BCM)
 for pin in (enc_pin_1, enc_pin_2, enc_button_pin, top_button_pin):
     GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 for pin in (enc_pin_1, enc_pin_2):
-    GPIO.add_event_detect(pin, GPIO.FALLING, callback=update_encoder)
-GPIO.add_event_detect(enc_button_pin, GPIO.FALLING, callback=enc_button_event)
-GPIO.add_event_detect(top_button_pin, GPIO.FALLING, callback=top_button_event)
+    GPIO.add_event_detect(pin, GPIO.FALLING, callback=update_encoder, bouncetime=5)
+GPIO.add_event_detect(enc_button_pin, GPIO.FALLING, callback=enc_button_event, bouncetime=500)
+GPIO.add_event_detect(top_button_pin, GPIO.FALLING, callback=top_button_event, bouncetime=500)
 
 
 def get_volume():
@@ -51,8 +53,6 @@ def get_volume():
 
 def playing(player):
     global enc_button_press, top_button_press, last_encoder_value
-#    music = "test.mp3"
-    music = "https://radio.stereoscenic.com/asp-s"
     player.loadfile(music)
     while not top_button_press:
         sleep(.5)
@@ -81,4 +81,9 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print(f"\ndone")
+    finally:
+        GPIO.cleanup() 
